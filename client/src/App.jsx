97c815f795167b2caffe8d7ec35792ffc24bb5b7
Tanya Remishevskaya <tanyaremishevskaya@MacBook-Air-Tanya.local>
2025-02-20 18:30:38 +0300

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,40 @@
 import Layout from './components/Layout';
 import MainPage from './components/pages/MainPage';
-// import { useState } from 'react';
+
 import { BrowserRouter, Route, Routes } from 'react-router';
 import LoginForm from './components/ui/LoginForm';
-import SignupPage from './components/ui/RegisterForm';
-import axiosInstance from './axiosInstance';
+import SignupPage from './components/ui/RegisterPage';
+import axiosInstance, { setAccessToken } from './axiosInstance';
 import { useState } from 'react';
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [user, setUser] = useState();
+  //   const signupHandler = async (event) => {
+  //     event.preventDefault();
+  //     const formData = new FormData(event.target)
+  //     const data = Object.fromEntries(formData);
+  //     const res = await axiosInstance.post('/auth/signup', data);
+  //     // обработка ответа res (сюда допишем позже)
+  // }
+
   const signupHandler = async (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target)
-    const data = Object.fromEntries(formData);
-    const res = await axiosInstance.post('/auth/signup', data);
-    // обработка ответа res (сюда допишем позже)
-}
-  const [count, setCount] = useState(0)
-
-    const signupHandler = async (event) => {
-    event.preventDefault();
-    const data =  Object.fromEntries(new FormData(event.target));
+    const data = Object.fromEntries(new FormData(event.target));
     const res = await axiosInstance.post('/auth/register', data);
     if (res.status !== 200) alert('Ошибка регистрации');
     setUser(res.data.user);
     setAccessToken(res.data.accessToken);
-}
+  };
 
-const loginHandler = async (e) => {
-  e.preventDefault();
-  const data = Object.fromEntries(new FormData(e.target));
-  console.log(data)
-  const res = await axiosInstance.post('/auth/login', data);
-  if (res.status !== 200) alert('Ошибка входа');
-  setUser(res.data.user);
-  setAccessToken(res.data.accessToken);
-};
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    const res = await axiosInstance.post('/auth/login', data);
+    if (res.status !== 200) alert('Ошибка входа');
+    setUser(res.data.user);
+    setAccessToken(res.data.accessToken);
+  };
 
   return (
     <BrowserRouter>
@@ -48,7 +42,10 @@ const loginHandler = async (e) => {
         <Route element={<Layout />}>
           <Route path="/" element={<MainPage />} />
           {/* <Route path="/signin" element={<LoginForm />} /> */}
-          <Route path="/register" element={<SignupPage signupHandler={signupHandler} />} />
+          <Route
+            path="/register"
+            element={<SignupPage signupHandler={signupHandler} />}
+          />
           <Route path="/login" element={<LoginForm loginHandler={loginHandler} />} />
         </Route>
       </Routes>
--- server/src/app.js
+++ server/src/app.js
@@ -16,6 +16,7 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use('/api/init', initiativRouter);
 app.use('/api/auth', authRouter);
 
