b78b071ade51a7abbbc0fcb917e0d2c953593251
KarenZakharian <zakharyankaren10@gmail.com>
2025-02-20 11:39:58 +0300

    initiavePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,38 @@
+import Layout from './components/Layout';
+import MainPage from './components/pages/MainPage';
+// import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import LoginForm from './components/ui/LoginForm';
+import SignupPage from './components/ui/RegisterForm';
+import axiosInstance from './axiosInstance';
+import { useState } from 'react';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [user, setUser] = useState();
+  const signupHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target)
+    const data = Object.fromEntries(formData);
+    const res = await axiosInstance.post('/auth/signup', data);
+    // обработка ответа res (сюда допишем позже)
+}
   const [count, setCount] = useState(0)
 
+import React from 'react';
+import TwoInit from './components/pages/twoinit'; 
+
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TwoInit />
     </>
-  )
+  );
 }
 
-export default App
+
+
