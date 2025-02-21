ffc77e9e22cf6bec37548ce08c848d744a5c7b8f
yanebylitsa <fable.prod@gmail.com>
2025-02-21 16:57:06 +0300

    all done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const path = require('path')
 
-process.env.NODE_EXTRA_CA_CERTS= path.resolve(__dirname, 'dir', 'with', 'certs')
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// <https://vitejs.dev/config/>
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-      '/chat': 'https://gigachat.devices.sberbank.ru/api/v1/',
-    },
-  },
   build: {
     outDir: '../server/dist',
   },
@@ -25,6 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
+      '/chat': 'https://gigachat.devices.sberbank.ru/api/v1/',
     },
   },
 });
