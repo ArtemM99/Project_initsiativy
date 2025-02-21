70daf5067fa28c59060df9e6a46f6b17225b8361
yanebylitsa <fable.prod@gmail.com>
2025-02-21 16:47:33 +0300

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const path = require('path')
+
+process.env.NODE_EXTRA_CA_CERTS= path.resolve(__dirname, 'dir', 'with', 'certs')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
 // <https://vitejs.dev/config/>
 export default defineConfig({
-plugins: [react()],
-server: {
-proxy: {
-"/api": "http://localhost:3000",
-},
-},
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/chat': 'https://gigachat.devices.sberbank.ru/api/v1/',
+    },
+  },
 });
+
 
--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const initiativRouter = require('./routes/initiativRouter');
 const authRouter = require('./routes/authRouter');
 
 const tokenRouter = require('./routes/tokenRouter');
+const aiRouter = require('./routes/aiRoute');
 // const cookieParser = require('cookie-parser');
 // const tokenRouter = require('./routes/tokenRouter');
 // const authRouter = require('./routes/authRouter');
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/init', initiativRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/ai', aiRouter);
 
 app.use('/api/tokens', tokenRouter);
 
